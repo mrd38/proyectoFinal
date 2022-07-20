2e531265eaffb2297d32caadaeefb1021de78537
MelanieDominguez <m3ldominguez3@gmail.com>
2022-07-20 01:33:30 -0300

    Desarrollo del fondo y otros elementos del header

--- portafolioWeb/src/app/app.module.ts
+++ portafolioWeb/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { FondoComponent } from './components/fondo/fondo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BannerComponent,
+    RedesComponent,
+    FondoComponent
   ],
   imports: [
     BrowserModule,
