d6c99f4f7ede05fc7d31d070d45ea2e786906e7f
MelanieDominguez <m3ldominguez3@gmail.com>
2022-07-21 02:20:05 -0300

    Agregacion de componente acercaDe

--- portafolioWeb/src/app/app.module.ts
+++ portafolioWeb/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { RedesComponent } from './components/redes/redes.component';
 import { FondoComponent } from './components/fondo/fondo.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FondoComponent } from './components/fondo/fondo.component';
     HeaderComponent,
     BannerComponent,
     RedesComponent,
-    FondoComponent
+    FondoComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
