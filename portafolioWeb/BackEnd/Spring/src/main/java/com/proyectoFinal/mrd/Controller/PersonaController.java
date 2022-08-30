3ee03b153f43b8748ddb3b5be42bd9c2c0b5d9c5
MelanieDominguez <m3ldominguez3@gmail.com>
2022-08-29 23:52:15 -0300

    mejoras

--- portafolioWeb/BackEnd/Spring/src/main/java/com/proyectoFinal/mrd/Controller/PersonaController.java
+++ portafolioWeb/BackEnd/Spring/src/main/java/com/proyectoFinal/mrd/Controller/PersonaController.java
@@ -55,10 +55,5 @@ public class PersonaController {
         return persona;
     }
     
-        @GetMapping("personas/traer/perfil")
-    public Persona findPersona(){
-        return ipersonaService.findPersona((long)1);
-    }
-  
-    
+ 
 }
